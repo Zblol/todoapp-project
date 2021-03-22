8a88d52a19cb58bb768a1208552cd6528c5c4666
Vladislav Artemov <42673508+Zblol@users.noreply.github.com>
2021-03-22 21:10:26 +0300

    added signup form

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 def signupuser(request):
-    return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
+    else:
+        User.objects.create_user(request.POST)
